0b36a698e480faa100fbf9d9baf64f4178674d11
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 19:27:01 -0300

    Escopo e inicialização

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,28 +5,14 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            /*Console.WriteLine("Entre com um número inteiro");
-            int x = int.Parse(Console.ReadLine());
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
 
-            if(x % 2 == 0) {
-                Console.WriteLine("Par!");
+            if (preco > 100.0) {
+                desconto = preco * 0.1;
             }
-            else {
-                Console.WriteLine("Impar!");
-            }*/
 
-            Console.WriteLine("Qual a hora atual");
-            int hora = int.Parse(Console.ReadLine());
-
-            if (hora < 12) {
-                Console.WriteLine("Bom dia!");
-            }
-            else if (hora < 18) {
-                Console.WriteLine("Boa tarde");
-            }
-            else {
-                Console.WriteLine("Boa noite");
-            }
+            Console.WriteLine(desconto);
 
         }
     }
