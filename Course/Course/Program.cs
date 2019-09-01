d335b3773b245e58c2477abd69ad3017069dc7ac
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 15:45:52 -0300

    Exercicio saida de dados

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,26 +5,24 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            char genero = 'F';
-            int idade = 32;
-            double saldo = 10.34543;
-            string nome = "Maria";
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritorio";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
 
-            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome,idade,saldo); //Usando playceholder
-            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Usando interpolação
-            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais"); //Usando concatenacao
-
-
-            /*Console.WriteLine("Bom Dia");
-            Console.WriteLine("--------------------");
-            Console.WriteLine(genero);
-            Console.WriteLine(idade);
-            Console.WriteLine(saldo);
-            Console.WriteLine(nome);
-            Console.WriteLine(saldo.ToString("F2"));
-            Console.WriteLine(saldo.ToString("F4"));
-            Console.WriteLine(saldo.ToString("F4",CultureInfo.InvariantCulture));*/
-
+            Console.WriteLine("Produtos");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: "+ medida.ToString("F2",CultureInfo.InvariantCulture));
 
         }
     }
