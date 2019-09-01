6099523026c8bb4190d5de5f36da08aa2c39ad04
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 19:04:47 -0300

    Operadores logicos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,27 +5,20 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int a = 10;
-            bool c1 = a < 10;
-            bool c2 = a < 20;
-            bool c3 = a > 10;
-            bool c4 = a > 5;
+            bool c1 = 2 > 3 && 4 != 5; //true
+            bool c2 = 2 > 3 || 4 != 5;
+            bool c3 = !(2 > 3) && 4 != 5; // true
 
             Console.WriteLine(c1);
             Console.WriteLine(c2);
             Console.WriteLine(c3);
+            Console.WriteLine("---------------");
+
+            bool c4 = 10 < 5; // false
+            bool c5 = c1 || c3 && c4; // true
+
             Console.WriteLine(c4);
-            Console.WriteLine("------------------");
-
-            bool c5 = a <= 10;
-            bool c6 = a >= 10;
-            bool c7 = a == 10;
-            bool c8 = a != 10;
-
             Console.WriteLine(c5);
-            Console.WriteLine(c6);
-            Console.WriteLine(c7);
-            Console.WriteLine(c8);
         }
     }
 }
