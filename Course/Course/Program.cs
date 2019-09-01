3f4a1a78589ff7232a8dc5387ee805dbb1e24709
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 16:29:52 -0300

    Operadores de atribuição

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,23 +5,23 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int n1 = 3 + 4 * 2;
-            int n2 = (3 + 4) * 2;
-            int n3 = 17 % 3;
-            double n4 = 10.0 / 8;
+            int a = 10;
+            Console.WriteLine(a);
+            a += 2;
+            Console.WriteLine(a);
+            a *= 3;
+            Console.WriteLine(a);
 
-            double a = 1.0 , b = -3.0, c = -4.0;
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            string s = "ABC";
+            Console.WriteLine(s);
+            s += "DEF";
+            Console.WriteLine(s);
 
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            int aa = 10;
+            int b = aa++;
+            Console.WriteLine(aa);
+            Console.WriteLine(b);
+
           
         }
     }
