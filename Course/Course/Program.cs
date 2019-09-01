1db5c57f2a7ed59926e79b3bd53ee6d99b77a8cd
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 15:27:24 -0300

    Saida de dados em C#

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,38 +1,29 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            bool completo = false;
             char genero = 'F';
-            char letra = '\u0041';
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 21436734673L;
-            float n5 = 4.5f;
-            double n6 = 4.6;
-            string nome = "Marcelo";
-            object obj1 = "Marcelo da Silva";
-            object obj2 = 4.5;
+            int idade = 32;
+            double saldo = 10.34543;
+            string nome = "Maria";
 
-            /*Console.WriteLine(completo);
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome,idade,saldo); //Usando playceholder
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Usando interpolação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais"); //Usando concatenacao
+
+
+            /*Console.WriteLine("Bom Dia");
+            Console.WriteLine("--------------------");
             Console.WriteLine(genero);
-            Console.WriteLine(letra);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
             Console.WriteLine(nome);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);*/
-
-            int n = int.MinValue;
-            decimal d = decimal.MaxValue;
-            Console.WriteLine(d);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4",CultureInfo.InvariantCulture));*/
 
 
         }
