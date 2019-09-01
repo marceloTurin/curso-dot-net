a2e871532af3a014c1044463dc25ed360bdb863a
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 15:07:53 -0300

    Tipos basicos de dados en C#

---
+++ Course/Course/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 21436734673L;
+            float n5 = 4.5f;
+            double n6 = 4.6;
+            string nome = "Marcelo";
+            object obj1 = "Marcelo da Silva";
+            object obj2 = 4.5;
+
+            /*Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);*/
+
+            int n = int.MinValue;
+            decimal d = decimal.MaxValue;
+            Console.WriteLine(d);
+
+
+        }
+    }
+}
