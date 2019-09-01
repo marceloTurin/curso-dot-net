e3ac11191b0848ff10c8c45a1f762c5ef901a9dd
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-09-01 18:19:32 -0300

    Exercicio entrada de dados em C#

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,23 +5,26 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int n1 = int.Parse(Console.ReadLine());
-            char ch = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            string[] vet = Console.ReadLine().Split(' ');
-            string nome = vet[0];
-            char sexo = char.Parse(vet[1]);
-            int idade = int.Parse(vet[2]);
-            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com seu nome completo");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa");
+            int qtdQuarto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto");
+            double precoProduto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");
+            string[] v = Console.ReadLine().Split(' ');
+            string nome = v[0];
+            int idade = int.Parse(v[1]);
+            double altura = double.Parse(v[2]);
 
-            Console.WriteLine("Você Digitou");
-            Console.WriteLine(n1);
-            Console.WriteLine(ch);
-            Console.WriteLine(n2.ToString("F2"),CultureInfo.InvariantCulture);
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(qtdQuarto);
+            Console.WriteLine(precoProduto.ToString("F2"),CultureInfo.InvariantCulture);
             Console.WriteLine(nome);
-            Console.WriteLine(sexo);
             Console.WriteLine(idade);
-            Console.WriteLine(altura.ToString("F2"),CultureInfo.InvariantCulture);
+            Console.WriteLine(altura.ToString("F2"));
+
+            //Console.WriteLine(altura.ToString("F2"),CultureInfo.InvariantCulture);
           
         }
     }
