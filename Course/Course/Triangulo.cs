84bfea39b197ee15bf5e8e7a98f6414f63124112
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-11-29 17:47:35 -0300

    Criando um método para obtermos os beneficios de reaproveitamento e delegação

--- Course/Course/Triangulo.cs
+++ Course/Course/Triangulo.cs
@@ -3,9 +3,16 @@
 
 namespace Course {
     class Triangulo {
+
+        //Atributos
         public double A;
         public double B;
         public double C;
 
+        //Metodos
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
