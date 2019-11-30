2b1b022f4fcbb0fcb8bd9fccad3ebf642042b217
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-11-30 14:47:34 -0300

    Encapsulamento

--- Course/Course/Produto.cs
+++ Course/Course/Produto.cs
@@ -3,48 +3,66 @@ using System.Globalization;
 
 namespace Course {
     class Produto {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         //Construtores de classe usando sobrecarga
         public Produto() {
-            Quantidade = 10;
+            _quantidade = 10;
         }
 
         public Produto(string nome, double preco) : this() {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
 
         }
         public Produto(string nome,double preco, int quantidade) : this(nome,preco) {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _quantidade = quantidade;
         }
 
         
         /********************************************/
 
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+
+            if(nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public double GetQuantidade() {
+            return _quantidade;
+        }
+
+
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         //Sobreposição da classe Object
         public override string ToString() {
-            return Nome
+            return _nome
                 + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidade, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
