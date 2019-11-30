5a4f0e543cff3ce6d03b4da92bdc28e2f6f37cf2
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-11-30 13:07:07 -0300

    Membros estaticos parte 2 - classe

---
+++ Course/Course/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Course {
+    class Calculadora {
+
+         public double Pi = 3.14;
+
+         public double Circunferencia(double raio) {
+            return 2.0 * Pi * raio;
+        }
+
+         public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,29 +5,26 @@ namespace Course {
     class Program {
         //Classes estaticas tem que ter atributos e classes estaticas;
 
-        static double Pi = 3.14;
+        
         static void Main(string[] args) {
+
+            Calculadora calc = new Calculadora();
+
+
 
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
 
             Console.WriteLine("Circunferencia: " + circ.ToString("F2",CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
 
         }
 
-        static double Circunferencia(double raio) {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double r) {
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
 
         
 
