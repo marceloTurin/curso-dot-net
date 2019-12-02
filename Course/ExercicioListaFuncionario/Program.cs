a7535ea312ff4c0318b409c26b5745d51981e601
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-02 18:15:04 -0300

    Listas (List)

---
+++ Course/ExercicioListaFuncionario/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioListaFuncionario {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
