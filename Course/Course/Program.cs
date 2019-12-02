c84839d44d358058855af2a79226ee6424df7e20
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-02 12:46:46 -0300

    Nullable parte 1

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,13 +4,30 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Point p;
-            p.X = 10;
-            p.Y = 20;
-            Console.WriteLine(p);
-            p = new Point();
-            Console.WriteLine(p);
+            //Nullable<double> x = null;
+            double? x = null;
+            double? y = 10.0;
+
+            //Pega o valor atual ou o default da variavel
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            //Verifica se a variavel tem valor retornando true ou false;
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if(x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
             
+            if(y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+
+
+
         }
     }
 }
