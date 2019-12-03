59ac0b94f5b12e323121292b01a8ad4ac91e699f
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-03 19:12:54 -0300

    Exercicio resolvido (matrizes)

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,10 +6,37 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            //Cria uma matriz
-            double[,] mat = new double[2, 3];
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(mat.Length);
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+
+            int count = 0;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(mat[i,j] < 0) {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Negative numbers: " + count);
 
         }
 
