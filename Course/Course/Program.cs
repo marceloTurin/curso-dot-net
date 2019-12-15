324d5211ebeab1bbd98cebed85f806dab8ebc41f
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-15 13:35:15 -0300

    Interferência de tipos: palavra var

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,35 +6,13 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            HashSet<int> A = new HashSet<int>();
-            HashSet<int> B = new HashSet<int>();
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
 
-            A.Add(5);
-            A.Add(3);
-            A.Add(8);
-            A.Add(9);
-
-            B.Add(3);
-            B.Add(4);
-            B.Add(5);
-
-            B.Remove(4);
-
-            A.ExceptWith(B);
-
-            foreach(int x in A){
-                Console.WriteLine(x);
-            }
-
-            Console.WriteLine("Digite um valor inteiro: ");
-            int N = int.Parse(Console.ReadLine());
-
-            if (B.Contains(N)) {
-                Console.WriteLine(N + " pertence ao conjunto B");
-            } else {
-                Console.WriteLine(N + " nao pertence ao conjunto B");
-            }
-
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
 
         }
 
