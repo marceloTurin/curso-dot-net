d83102673ba503179338d178847a87fd77fbb425
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-15 14:01:01 -0300

    Expressao condicional ternaria

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,38 +6,11 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int x = int.Parse(Console.ReadLine());
-            string day;
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.00) ? preco * 0.1 : preco * 0.85;
 
-            switch (x) {
-                case 1:
-                    day = "Sunday";
-                    break;
-                case 2:
-                    day = "Monday";
-                    break;
-                case 3:
-                    day = "Tuesday";
-                    break;
-                case 4:
-                    day = "Wednesday";
-                    break;
-                case 5:
-                    day = "Thursday";
-                    break;
-                case 6:
-                    day = "Friday";
-                    break;
-                case 7:
-                    day = "Saturday";
-                    break;
-                default:
-                    day = "Invalid value";
-                    break;
-
-            }
-
-            Console.WriteLine("Day: "+ day);
+            Console.WriteLine(desconto);
+            
 
         }
 
