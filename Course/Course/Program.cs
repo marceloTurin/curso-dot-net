52b59245af4a1ffcc8933f71ae422fe799bb1842
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-15 13:52:19 -0300

    Switch case

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -6,13 +6,38 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            var x = 10;
-            var y = 20.0;
-            var z = "Maria";
+            int x = int.Parse(Console.ReadLine());
+            string day;
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            switch (x) {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+
+            }
+
+            Console.WriteLine("Day: "+ day);
 
         }
 
