b53ba32fd4272c4972b5275f6f8f77962079ef7f
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-17 15:57:22 -0300

    Enumerações

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,34 +1,29 @@
 ﻿using System;
 using System.Globalization;
 using System.Collections.Generic;
+using Course.Entities;
+using Course.Entities.Enums;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            TimeSpan t1 = new TimeSpan();
-            TimeSpan t2 = new TimeSpan(90000000);
-            TimeSpan t3 = new TimeSpan(2, 11, 11);
-            TimeSpan t5 = new TimeSpan(1, 2, 11, 11);
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
 
-            TimeSpan t6 = TimeSpan.FromDays(1.5);
-            TimeSpan t7 = TimeSpan.FromHours(1.5);
-            TimeSpan t8 = TimeSpan.FromMinutes(1.5);
-            TimeSpan t9 = TimeSpan.FromSeconds(1.5);
-            TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
-            TimeSpan t11 = TimeSpan.FromTicks(9000000000L);
+            Console.WriteLine(order);
 
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
-            Console.WriteLine(t3);
-            Console.WriteLine(t5);
-            Console.WriteLine(t6);
-            Console.WriteLine(t7);
-            Console.WriteLine(t8);
-            Console.WriteLine(t9);
-            Console.WriteLine(t10);
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
 
 
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        
 
         }
 
