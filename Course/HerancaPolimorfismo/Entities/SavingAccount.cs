6867e983e787e1ed6e5a269eabd00238eb22ac7a
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-21 21:47:46 -0300

    Upcasting e downcasting

---
+++ Course/HerancaPolimorfismo/Entities/SavingAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaPolimorfismo.Entities {
+    class SavingAccount : Account {
+
+        public double InteresRate { get; set; }
+
+        public SavingAccount () {
+
+        }
+
+        public SavingAccount(int number, string holder, double balance,double interesRate) : base(number,holder,balance) {
+            InteresRate = interesRate;
+        }
+
+        public void UpdateBalance() {
+            Balance += Balance * InteresRate;
+        }
+
+    }
+}
--- Course/HerancaPolimorfismo/Program.cs
+++ Course/HerancaPolimorfismo/Program.cs
@@ -5,10 +5,38 @@ namespace HerancaPolimorfismo {
     class Program {
         static void Main(string[] args) {
 
-            BusinessAcount account = new BusinessAcount(8010, "Bob brown", 100.0, 500.0);
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAcount bacc = new BusinessAcount(1002, "Maria", 0.0, 500.00);
 
-            //account.Balance = 200;
+
+            // UPCATING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAcount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            BusinessAcount acc4 = (BusinessAcount)acc2;
+            acc4.Loan(100);
+
+
+            //BusinessAcount acc5 = (BusinessAcount)acc3;
+
+            if(acc3 is BusinessAcount) {
+                // BusinessAcount acc5 = (BusinessAcount)acc3;
+                BusinessAcount acc5 = acc3 as BusinessAcount;
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingAccount) {
+                //SavingAccount acc5 = (SavingAccount)acc3;
+                SavingAccount acc5 = acc3 as SavingAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
         }
     }
 }
