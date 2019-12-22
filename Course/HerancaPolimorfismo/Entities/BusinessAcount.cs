b676b3a3758a61cf5846ee0788be8b834dc2d0c2
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-21 21:23:53 -0300

    Herança

---
+++ Course/HerancaPolimorfismo/Entities/BusinessAcount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaPolimorfismo.Entities {
+    class BusinessAcount : Account {
+
+        public double LoanLimit { get; set; }
+
+        public BusinessAcount() {
+
+        }
+
+        public BusinessAcount(int number, string holder, double balance,double loanLimit) :base(number,holder,balance) {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) {
+
+            if(amount <= LoanLimit) {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Course/HerancaPolimorfismo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HerancaPolimorfismo.Entities;
+
+namespace HerancaPolimorfismo {
+    class Program {
+        static void Main(string[] args) {
+
+            BusinessAcount account = new BusinessAcount(8010, "Bob brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200;
+        }
+    }
+}
