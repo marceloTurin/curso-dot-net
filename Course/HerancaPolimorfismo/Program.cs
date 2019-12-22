75510ac686e53c42fcd0c734ab496d3766092a52
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-22 12:57:56 -0300

    Exercicio resolvido - polimorfismo

--- Course/HerancaPolimorfismo/Program.cs
+++ Course/HerancaPolimorfismo/Program.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using HerancaPolimorfismo.Entities;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace HerancaPolimorfismo {
     class Program {
         static void Main(string[] args) {
 
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingAccount(1002, "Anna", 500.0, 0.01);
+            List<Employee> list = new List<Employee>();
 
-            acc1.Withdraw(10.0);
-            acc2.Withdraw(10.0);
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsorced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("Value per hours");
+                double valuePerHours = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+                if(ch == 'y') {
+                    Console.WriteLine("Additional charge:");
+                    double additionalCharge = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHours, additionalCharge));
+                }
+                else {
+                    list.Add(new Employee(name, hours, valuePerHours));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+
+            foreach(Employee emp in list) {
+                Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2",CultureInfo.InvariantCulture)}");
+            }
 
         }
     }
