9d37a0b5c7fc4cf9fef076bb359950af631e0073
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-30 18:38:13 -0300

    Solução sem interface - PARTE 1

---
+++ Course/Interface/Entities/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Interface.Entities {
+    class Invoice {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax) {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString() {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + "\nTotal payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Interface/Services/RentalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Interface.Entities;
+
+namespace Interface.Services {
+    class RentalService {
+
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay) {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental) {
+
+        }
+    }
+}
