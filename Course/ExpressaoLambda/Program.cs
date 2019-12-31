46268e62bde43c69d09bb3ec14834c5b1fb55ea4
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-31 15:18:57 -0300

    Introdução a delegates

---
+++ Course/ExpressaoLambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExpressaoLambda.Services;
+using System;
+
+namespace ExpressaoLambda {
+    class Program {
+
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args) {
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
--- Course/ExtensionMethods/Program.cs
+++ Course/ExtensionMethods/Program.cs
@@ -8,7 +8,7 @@ namespace ExtensionMethods {
             Console.WriteLine(dt.ElapsedTime());
 
             string s1 = "Good morning dear students!";
-            Console.WriteLine(s1.Cut(10))
+            Console.WriteLine(s1.Cut(10));
 
         }
     }
