5773e05d3e80d220bb5ef86833f6d6560f481429
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-31 18:05:13 -0300

    Exercício resolvido : Expressão lambda

---
+++ Course/ExpressaoLambda/Entities/ProductNew.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressaoLambda.Entities {
+    class ProductNew {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public ProductNew(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- Course/ExpressaoLambda/Program.cs
+++ Course/ExpressaoLambda/Program.cs
@@ -3,101 +3,38 @@ using ExpressaoLambda.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Globalization;
 
 namespace ExpressaoLambda {
     class Program {
 
-        static void Print<T>(string message,IEnumerable<T> collection) {
-            Console.WriteLine(message);
-            foreach(T obj in collection) {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine();
-        }
-
         static void Main(string[] args) {
 
-            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
-            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
-            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
 
-            List<Product> products = new List<Product>() {
-                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
-                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
-                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
-                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
-                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
-                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
-                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
-                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
-                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
-                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
-                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
-            };
+            List<ProductNew> list =  new List<ProductNew>();
 
-            // var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
-            var r1 =
-                from p in products
-                where p.Category.Tier == 1 && p.Price < 900.0
-                select p;
-            Print("TIER 1 AND PRICE < 900:", r1);
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+                    string[] fields = sr.ReadLine().Split(',');
 
-            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
-            var r2 =
-                from p in products
-                where p.Category.Name == "Tools"
-                select p.Name;
-            Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+                    string name = fields[0];
+                    double price = double.Parse(fields[1],CultureInfo.InvariantCulture);
 
-            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
-            var r3 =
-                from p in products
-                where p.Name[0] == 'C'
-                select new {
-                    p.Name,
-                    p.Price,
-                    CategoryName = p.Name
-                };
-            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
-
-            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
-            var r4 =
-                from p in products
-                where p.Category.Tier == 1
-                orderby p.Name
-                orderby p.Price
-                select p;
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
-
-            //Pule os dois primeiros e pega os restante
-            //var r5 = r4.Skip(2).Take(4);
-            var r5 =
-                (from p in r4
-                 select p)
-                 .Skip(2)
-                 .Take(4);
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
-
-
-
-
-            //var r16 = products.GroupBy(p => p.Category);
-            var r16 =
-                from p in products
-                group p by p.Category;
-
-            Console.WriteLine();
-            foreach (IGrouping<Category,Product> group in r16) {
-                Console.WriteLine("Category " + group.Key.Name + ":");
-
-                foreach (Product p in group) {
-                    Console.WriteLine(p);
+                    list.Add(new ProductNew(name, price));
                 }
-                Console.WriteLine();
             }
 
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Average price: {avg.ToString("F2",CultureInfo.InvariantCulture)} ");
 
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
 
+            foreach (string name in names) {
+                Console.WriteLine(name);
+            }
 
 
         }
