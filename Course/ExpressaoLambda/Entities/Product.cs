b09b924539e9ddd89c5941cc7082e1c8919d29e4
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2019-12-31 17:08:03 -0300

    Demo - LINQ com Lambda - PARTE 1

--- Course/ExpressaoLambda/Entities/Product.cs
+++ Course/ExpressaoLambda/Entities/Product.cs
@@ -2,16 +2,22 @@
 
 namespace ExpressaoLambda.Entities {
     class Product {
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public Category Category { get; set; }
 
-        public Product(string name, double price) {
-            Name = name;
-            Price = price;
-        }
 
         public override string ToString() {
-            return Name + ", " + Price.ToString("F2");
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2")
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
         }
     }
 }
